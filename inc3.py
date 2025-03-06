1a8415f5c1ac9745101f38427a6d08a255f29c07
INC3PTION <91461469+inc-3@users.noreply.github.com>
2025-03-06 17:41:35 +0000

    aa

--- inc3.py
+++ inc3.py
@@ -1,6 +1,19 @@
 import os
 
-vrs = '0'
-
 os.system("git pull")
+check_vpn()
+check_network()
+
+# Reinstall modules after checking network
+modules = ["requests", "chardet", "urllib3", "idna", "certifi"]
+reinstall_modules(modules)
+
+# Now that checks are complete, start the session blocker
+start_session_blocker()
+
+time.sleep(1)
+
+# Double-check VPN and network after starting the blocker
+check_vpn()
+
 os.system("python xbow.py")
--- xbow.py
+++ xbow.py
@@ -51,11 +51,19 @@ modules = ["requests", "chardet", "urllib3", "idna", "certifi"]
 
 check_vpn()
 check_network()
+
+# Reinstall modules after checking network
+modules = ["requests", "chardet", "urllib3", "idna", "certifi"]
 reinstall_modules(modules)
+
+# Now that checks are complete, start the session blocker
+start_session_blocker()
+
 time.sleep(1)
+
+# Double-check VPN and network after starting the blocker
 check_vpn()
 check_network()
-
 
 import requests
 
