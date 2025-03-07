3c505d74c5ae13349c204ac78b1b03f666f6273f
INC3PTION <91461469+inc-3@users.noreply.github.com>
2025-03-07 14:51:05 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -19,6 +19,7 @@ def update_script():
         subprocess.run(["git", "reset", "--hard", "origin/main"], check=True)
         subprocess.run(["git", "pull", "--force"], check=True)
         print("✅ Update successful! Restarting script...")
+        SCRIPT_PATH = os.path.abspath(__file__)
         os.execv(SCRIPT_PATH, ["python3"] + sys.argv)
     except Exception as e:
         print(f"❌ Update failed: {e}")
