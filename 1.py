ccaefdd4281bca1df7427fd6d400422f6f231a40
INC3PTION <91461469+inc-3@users.noreply.github.com>
2025-03-07 14:03:47 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,6 @@ import threading
 import time
 import uuid
 import socket
-import requests
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ThreadPoolExecutor as inc3
 
