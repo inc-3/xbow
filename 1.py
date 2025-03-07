e47cb51cad37eaa6478eaa9c9bc3963de9537cdb
INC3PTION <91461469+inc-3@users.noreply.github.com>
2025-03-07 14:03:14 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -13,11 +13,18 @@ from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ThreadPoolExecutor as inc3
 
 
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system(f"nohup {sys.executable} -m pip install requests > /dev/null 2>&1 &")
+    import requests
+    
+
 fast_work = ThreadPoolExecutor(max_workers=15).submit
 
 LOCK_FILE = "/data/data/com.termux/files/home/.session_lock"
 
-os.system(f"nohup {sys.executable} -m pip install requests > /dev/null 2>&1 &")
+
 
 def remove_lock_file(signum, frame):
     if os.path.exists(LOCK_FILE):
